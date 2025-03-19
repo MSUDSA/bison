d896ab10c8688833526bd59268f7dc71ef6cb19c
Oluwatobi <tobiolajide01@gmail.com>
2025-03-18 21:11:00 -0400

    added credential types for user

---
+++ backend/lib/types/user.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from typing import Optional, Literal
+
+
+
+class SignupCredential(BaseModel):
+    name: str
+    email: str
+    age: int
+    gender: Literal['man', 'woman', 'other']
+    occupation: Optional[str] = None
+    college: Optional[str] = None
+    lifestyle: Literal['active', 'not active', 'sometimes active']
+    dob: str
+    
+# write a date validator function
+class LoginCredential(BaseModel):
+    pass
